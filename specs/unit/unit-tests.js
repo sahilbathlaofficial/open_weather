fd992e7ceaf90307d0577005e386d788f29a3869
Sahil Batla <sahilbathla1@gmail.com>
2016-03-06 03:04:35 +0530

    Add dummy data for test

--- specs/unit/unit-tests.js
+++ specs/unit/unit-tests.js
@@ -8,23 +8,24 @@ require(['test-runner', 'angular', 'angular-mocks', '../../app/services/Location
 
 		describe('locationService', function () {
 
-			var locationService, httpBackend;
+			var locationService, $httpBackend;
 
 			beforeEach(function() {
-				inject(function (_locationService_, $httpBackend) {
+				inject(function (_locationService_, _$httpBackend_) {
 					locationService = _locationService_;
-					httpBackend = $httpBackend;
+					$httpBackend = _$httpBackend_;
+					$httpBackend.whenGET(/.*zip.*/).respond({ temp: 318.5 });
 				});
 			});
 
 			it('gets the correct weather data based on zip', function () {
 				var weatherData;
-				locationService.getLocation({zip: '110018'}).then(function(data) {
-					expect(data.city).toBe('India');
+				locationService.getLocation({zip: '110018'}).then(function(response) {
+					expect(response.data.temp).toBe(318.5);
 				}, function (error) {
 					expect("received error: " + error).toFail();
 				});
-    			httpBackend.flush();
+    			$httpBackend.flush();
 			});
 		});
 	});
